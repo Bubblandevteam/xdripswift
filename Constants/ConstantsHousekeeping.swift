989cb5ace8e627227451e478e197a97991957871
Bubblan <83052220+Bubblandevteam@users.noreply.github.com>
2021-10-21 14:38:14 +0200

    Delete Constants directory

--- Constants/ConstantsHousekeeping.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum ConstantsHousekeeping {
-    
-    /// how long to keep bgReadings can calibrations
-    static let retentionPeriodBgReadingsAndCalibrationsInDays = 90.0
-    
-}
--- Constants/ConstantsWatch.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum ConstantsWatch {
-    
-    /// text to add as notes in glucose events
-    static let textInCreatedEvent = "created by xdrip"
-    
-}
